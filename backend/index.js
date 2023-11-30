992a41e21f449bb0d0fce835adb41fad07bc81fc
Sanjay Pathak <sanjaywrongcode@gmail.com>
2023-11-30 11:59:24 +0530

    render

--- backend/index.js
+++ backend/index.js
@@ -27,11 +27,10 @@ app.use("/api/message", messageRoutes);
 
 // -------------------Deployment Start---------------------
 const __dirname1 = path.resolve();
-console.log(path.join(__dirname1, "../frontend/build/index.html"));
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname1, '../frontend/build')));
+    app.use(express.static(path.join(__dirname1, '/frontend/build')));
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname1, "../frontend/build/index.html"));
+        res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
     })
 } else {
     app.get("/", (req, res) => {
